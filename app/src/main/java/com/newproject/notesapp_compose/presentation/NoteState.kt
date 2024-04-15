9799caabe5872ba3089de67e91507b8bdfffab51
Abdul Basit <basitbhattee@gmail.com>
2024-04-15 09:17:48 +0500

    Worked on MVVM.

--- app/src/main/java/com/newproject/notesapp_compose/presentation/NoteState.kt
+++ app/src/main/java/com/newproject/notesapp_compose/presentation/NoteState.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.newproject.notesapp_compose.db.Note
 
-class NoteState {
-    val notes : List<Note> = emptyList()
-    val title : MutableState<String> = mutableStateOf("")
+class NoteState (
+    val notes : List<Note> = emptyList(),
+    val title : MutableState<String> = mutableStateOf(""),
     val description : MutableState<String> = mutableStateOf("")
-}
+)
--- app/src/main/java/com/newproject/notesapp_compose/presentation/NoteViewModel.kt
+++ app/src/main/java/com/newproject/notesapp_compose/presentation/NoteViewModel.kt
@@ -1,11 +1,43 @@
 package com.newproject.notesapp_compose.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.newproject.notesapp_compose.db.NoteDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 
 class NoteViewModel(
     val dao : NoteDao
 ) : ViewModel() {
 
+    private var isSortedByDateAdded = MutableStateFlow(true)
+
+    private var notes = isSortedByDateAdded.flatMapLatest {
+        if (it){
+            dao.GetNotesByDate()
+        } else {
+            dao.GetNotesByTitle()
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
+
+    var _state = MutableStateFlow(NoteState())
+    var state = combine(_state, isSortedByDateAdded, notes){
+        state, isSortedByDateAdded, notes ->
+        state.copy(
+            notes = notes
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteState())
+
+
+    fun onEvent(event:NoteEvent){
+        when(event){
+            is NoteEvent.DeleteNote -> TODO()
+            NoteEvent.SortNotes -> TODO()
+        }
+    }
 
 }
